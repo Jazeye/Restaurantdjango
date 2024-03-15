75b52df37ebf58a3807ce553fc71b75ea9e7475b
Jazeye <jaseelp394@gmail.com>
2024-03-15 07:50:56 +0530

    linked

--- reservation_project/reservation/views.py
+++ reservation_project/reservation/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def reservationtable_view(request):
+    return render(request, 'reservationtable.html')
+
+# def menu(request):
+#     return render(request, 'menu.html')
